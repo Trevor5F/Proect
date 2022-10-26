bcae04612a2ef10948b52ed3b5fd9c383dc7fe20
Admin <Dexter2366@yandex.ru>
2022-10-26 21:02:06 +0300

    proect 1.4

---
+++ todolist/goals/serializer.py
@@ -0,0 +1,85 @@
+from typing import Type
+
+from rest_framework import serializers
+
+from core.serializers import ProfileSerializer
+from goals.models import GoalCategory, Goal, GoalComment
+
+
+class GoalCategoryCreateSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = GoalCategory
+        read_only_fields = ("id", "created", "updated", "user")
+        fields = "__all__"
+
+
+
+class GoalCategorySerializer(serializers.ModelSerializer):
+    user = ProfileSerializer(read_only=True)
+
+    class Meta:
+        model = GoalCategory
+        fields = "__all__"
+        read_only_fields = ("id", "created", "updated", "user")
+
+
+class GoalCreateSerializer(serializers.ModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(queryset=GoalCategory.objects.filter(is_deleted=False))
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Goal
+        fields = "__all__"
+        read_only_fields = ("id", "created", "updated", "user")
+
+
+    def validate_category(self, value: Type[GoalCategory]):
+        if value.is_deleted:
+            raise serializers.ValidationError("not allowed in deleted category")
+
+        if value.user != self.context["request"].user:
+            raise serializers.ValidationError("not owner of category")
+
+        return value
+
+
+class GoalSerializer(serializers.ModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=GoalCategory.objects.filter(is_deleted=False)
+    )
+
+    class Meta:
+        model = Goal
+        fields = "__all__"
+        read_only_fields = ("id", "created", "updated", "user")
+
+
+    def validate_category(self, value: Type[GoalCategory]):
+        if value.is_deleted:
+            raise serializers.ValidationError("not allowed in deleted category")
+
+        if value.user != self.context["request"].user:
+            raise serializers.ValidationError("not owner of category")
+
+        return value
+
+
+
+class GoalCommentCreateSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = GoalComment
+        fields = "__all__"
+        read_only_fields = ("id", "created", "updated", "user")
+
+
+class GoalCommentSerializer(serializers.ModelSerializer):
+    user = ProfileSerializer(read_only=True)
+
+    class Meta:
+        model = GoalComment
+        fields = "__all__"
+        read_only_fields = ("id", "created", "updated", "user", "goal")
